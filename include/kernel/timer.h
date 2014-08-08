6036364844c424642bd582cf4f2d9483a28c1dc8
Baptiste Covolato <b.covolato@gmail.com>
2014-08-08 13:50:13 +0200

    [KERNEL][TIMER] Add timer_reserve() and timer update mechanism

--- include/kernel/timer.h
+++ include/kernel/timer.h
@@ -1,17 +1,38 @@
 #ifndef TIMER_H
 # define TIMER_H
 
+# include <kernel/types.h>
+# include <kernel/klist.h>
+
 /* 1 ms */
 # define TIMER_GRANULARITY 1
+
+# define TIMER_NONE 0
+# define TIMER_CALLBACK (1 << 1)
+# define TIMER_MESSAGE (1 << 2)
+# define TIMER_ONE_SHOT (1 << 3)
+# define TIMER_INFINITE (1 << 4)
 
 struct timer_glue
 {
     void (*init)(void);
 };
 
+struct timer_entry
+{
+    int type;
+    int data;
+    size_t timer;
+    size_t count;
+    void (*callback)(int);
+
+    struct klist list;
+};
+
 extern struct timer_glue __timer;
 
 void timer_initialize(void);
 void timer_handler(int irq, int data);
+int timer_register(int type, int data, size_t time, void (*callback)(int));
 
 #endif /* !TIMER_H */
--- kernel/core/timer.c
+++ kernel/core/timer.c
@@ -1,12 +1,70 @@
 #include <kernel/timer.h>
+#include <kernel/panic.h>
+#include <kernel/kmalloc.h>
+
+static struct klist timers;
 
 void timer_initialize(void)
 {
     __timer.init();
+
+    klist_head_init(&timers);
 }
 
 void timer_handler(int irq, int data)
 {
     (void) irq;
     (void) data;
+
+    struct timer_entry *timer;
+
+    klist_for_each_elem(&timers, timer, list)
+    {
+        --timer->count;
+
+        if (!timer->count)
+        {
+            if (timer->type & TIMER_CALLBACK)
+                timer->callback(timer->data);
+            else
+                kernel_panic("Timer message not implemented");
+
+            if (timer->type & TIMER_INFINITE)
+                timer->count = timer->timer;
+            else
+            {
+                klist_del(&timer->list);
+                kfree(timer);
+            }
+        }
+    }
+}
+
+int timer_register(int type, int data, size_t time, void (*callback)(int))
+{
+    struct timer_entry *timer;
+
+    if (!(type & TIMER_CALLBACK) && !(type & TIMER_MESSAGE))
+        return 0;
+
+    if (!(type & TIMER_ONE_SHOT) && !(type & TIMER_INFINITE))
+        return 0;
+
+    if ((type & TIMER_CALLBACK) && !callback)
+        return 0;
+
+    if (!time)
+        return 0;
+
+    timer = kmalloc(sizeof (struct timer_entry));
+
+    timer->type = type;
+    timer->data = data;
+    timer->timer = time / TIMER_GRANULARITY;
+    timer->count = time / TIMER_GRANULARITY;
+    timer->callback = callback;
+
+    klist_add(&timers, &timer->list);
+
+    return 1;
 }
