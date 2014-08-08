e615f1ccb576b189d21af26428067e41e3103330
Baptiste Covolato <b.covolato@gmail.com>
2014-08-08 12:54:30 +0200

    [I386] Add inb()

--- include/kernel/arch/i386/arch/io.h
+++ include/kernel/arch/i386/arch/io.h
@@ -11,4 +11,15 @@ static inline void outb(uint16_t port, uint8_t data)
                          , "Nd" (port));
 }
 
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t data;
+
+    __asm__ __volatile__("inb %w1, %b0"
+                         : "=a" (data)
+                         : "Nd" (port));
+
+    return data;
+}
+
 #endif /* !X86_IO_H */
