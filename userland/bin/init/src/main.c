bd8a1dbca0342ed14adbc25444f51ec2c65ef949
Baptiste Covolato <b.covolato@gmail.com>
2014-08-12 13:55:17 +0200

    [USERLAND][VFS & INIT] Now use a print syscall for debug purpose

--- userland/bin/init/src/main.c
+++ userland/bin/init/src/main.c
@@ -1,7 +1,25 @@
 int main(void)
 {
+    char message[5];
+
+    message[0] = 'I';
+    message[1] = 'N';
+    message[2] = 'I';
+    message[3] = 'T';
+    message[4] = 0;
+
     while (1)
-        ;
+    {
+        for (volatile int i = 0; i < 10000000; ++i)
+            ;
+
+            __asm__ __volatile__("mov %0, %%ebx\n"
+                                 "mov $1, %%eax\n"
+                                 "int $0x80\n"
+                                 :
+                                 : "r" (message)
+                                 : "memory");
+    }
 
     return 0;
 }
--- userland/driver/vfs/src/main.c
+++ userland/driver/vfs/src/main.c
@@ -1,7 +1,24 @@
 int main(void)
 {
+    char message[4];
+
+    message[0] = 'V';
+    message[1] = 'F';
+    message[2] = 'S';
+    message[3] = 0;
+
     while (1)
-        ;
+    {
+        for (volatile int i = 0; i < 10000000; ++i)
+            ;
+
+            __asm__ __volatile__("mov %0, %%ebx\n"
+                                 "mov $1, %%eax\n"
+                                 "int $0x80\n"
+                                 :
+                                 : "r" (message)
+                                 : "memory");
+    }
 
     return 0;
 }
