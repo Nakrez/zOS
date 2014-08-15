d0824b08398da1778dd755567b914f78411dfce0
Baptiste Covolato <b.covolato@gmail.com>
2014-08-15 22:31:42 +0200

    [I386] Page fault handler is now able to detect COW

--- kernel/arch/i386/page_fault.c
+++ kernel/arch/i386/page_fault.c
@@ -2,6 +2,7 @@
 #include <kernel/console.h>
 #include <kernel/thread.h>
 #include <kernel/process.h>
+#include <kernel/segment.h>
 
 #include <arch/page_fault.h>
 
@@ -24,16 +25,37 @@ void page_fault_handler(struct irq_regs *regs)
     if (regs->irq_data & PAGE_FAULT_USER)
     {
         if (regs->irq_data & PAGE_FAULT_WRITE)
-            kernel_panic("page_fault_handler: check COW needed");
-        else
         {
-            console_message(T_ERR, "Process %i: page fault");
+            struct as_mapping *mapping;
 
-            process_exit(thread->parent, PROCESS_CODE_SEGV);
+            mapping = as_mapping_locate(thread->parent->as, addr_fault);
+
+            if (mapping && mapping->phy->flags & SEGMENT_FLAGS_COW)
+            {
+                kernel_panic("page_fault_handler: user COW implem needed");
+
+                return;
+            }
         }
+
+        console_message(T_ERR, "Process %i: page fault", thread->parent->pid);
+
+        process_exit(thread->parent, PROCESS_CODE_SEGV);
     }
     else
     {
+        /* Check if a COW is needed (for page table) */
+        if (regs->irq_data & PAGE_FAULT_WRITE && addr_fault >= 0xFFC00000)
+        {
+            struct segment *seg;
+            paddr_t pt = as_virt_to_phy(addr_fault);
+
+            seg = segment_locate(pt);
+
+            if (seg && seg->flags & SEGMENT_FLAGS_COW)
+                kernel_panic("page_fault_handler: kernel COW implem needed");
+        }
+
         console_message(T_ERR, "KERNEL PAGE FAULT");
 
         console_message(T_ERR, "Faulting address: 0x%x", addr_fault);
