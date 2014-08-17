64227fa641b75041de7a351aa03e52af3f494c6d
Baptiste Covolato <b.covolato@gmail.com>
2014-08-17 22:07:20 +0200

    [I386] Page fault handler is now able to deal with COW

--- kernel/arch/i386/page_fault.c
+++ kernel/arch/i386/page_fault.c
@@ -1,3 +1,6 @@
+#include <string.h>
+
+#include <kernel/zos.h>
 #include <kernel/panic.h>
 #include <kernel/console.h>
 #include <kernel/thread.h>
@@ -5,6 +8,43 @@
 #include <kernel/segment.h>
 
 #include <arch/page_fault.h>
+#include <arch/mmu.h>
+
+static void cow(struct as *as, struct as_mapping *mapping, vaddr_t addr_fault)
+{
+    if (mapping->phy->ref_count == 1)
+        mapping->phy->flags &= ~SEGMENT_FLAGS_COW;
+    else
+    {
+        /* Allocate new physical memory */
+        struct segment *seg = segment_alloc(mapping->size / PAGE_SIZE);
+
+        uint32_t *new_page;
+        uint32_t *old_page = (uint32_t*)(align(addr_fault, PAGE_SIZE) -
+                                         PAGE_SIZE);
+
+        if (!seg)
+            kernel_panic("COW: Out of memory");
+
+        /* Copy memory area */
+        new_page = (uint32_t *)as_map(&kernel_as, 0, seg->base,
+                                      mapping->size / PAGE_SIZE, AS_MAP_WRITE);
+
+        if (!new_page)
+            kernel_panic("COW: Unable to copy");
+
+        memcpy(new_page, old_page, mapping->size);
+
+        as_unmap(&kernel_as, (vaddr_t)new_page, AS_UNMAP_NORELEASE);
+
+        /* Since we duplicated the pages there is one less reference */
+        --mapping->phy->ref_count;
+    }
+
+    /* Remap with old rights */
+    if (!as_remap(as, mapping, mapping->flags))
+        kernel_panic("COW: unable to remap memory area");
+}
 
 void page_fault_handler(struct irq_regs *regs)
 {
@@ -32,7 +72,7 @@ void page_fault_handler(struct irq_regs *regs)
 
             if (mapping && mapping->phy->flags & SEGMENT_FLAGS_COW)
             {
-                kernel_panic("page_fault_handler: user COW implem needed");
+                cow(thread->parent->as, mapping, addr_fault);
 
                 return;
             }
@@ -45,22 +85,6 @@ void page_fault_handler(struct irq_regs *regs)
     }
     else
     {
-        /* Check if a COW is needed (for page table) */
-        if (regs->irq_data & PAGE_FAULT_WRITE && addr_fault >= 0xFFC00000)
-        {
-            struct segment *seg;
-            paddr_t pt = as_virt_to_phy(addr_fault);
-
-            seg = segment_locate(pt);
-
-            if (seg && seg->flags & SEGMENT_FLAGS_COW)
-            {
-                kernel_panic("page_fault_handler: kernel COW implem needed");
-
-                return;
-            }
-        }
-
         console_message(T_ERR, "KERNEL PAGE FAULT");
 
         console_message(T_ERR, "Faulting address: 0x%x", addr_fault);
