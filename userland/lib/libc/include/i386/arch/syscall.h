1ca258da4cef069d3d408d0dcca12b226cd6b973
Baptiste Covolato <b.covolato@gmail.com>
2014-08-17 22:10:38 +0200

    [I386][USERLAND][LIBC] Add SYSCALL0 macro

--- userland/lib/libc/include/i386/arch/syscall.h
+++ userland/lib/libc/include/i386/arch/syscall.h
@@ -3,6 +3,13 @@
 
 # define SYS_USLEEP 2
 # define SYS_EXIT 3
+# define SYSCALL0(num, ret)                 \
+    __asm__ __volatile__("mov %1, %%eax\n"  \
+                         "int $0x80\n"      \
+                         "mov %%eax, %0\n"  \
+                         : "=r" (ret)       \
+                         : "i" (num)        \
+                         : "memory");
 
 # define SYSCALL1(num, arg1, ret)           \
     __asm__ __volatile__("mov %2, %%ebx\n"  \
