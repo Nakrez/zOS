4948761f035296c42096c875562df4927f247b56
Baptiste Covolato <b.covolato@gmail.com>
2014-08-18 22:09:33 +0200

    [I386][USERLAND][LIBC] Add SYSCALL3 macro

--- userland/lib/libc/include/i386/arch/syscall.h
+++ userland/lib/libc/include/i386/arch/syscall.h
@@ -5,25 +5,40 @@
 # define SYS_EXIT 3
 # define SYS_FORK 4
 # define SYS_GETPID 5
+# define SYS_THREAD_CREATE 6
 # define SYS_THREAD_EXIT 7
 # define SYS_GETTID 8
 
-# define SYSCALL0(num, ret)                 \
-    __asm__ __volatile__("mov %1, %%eax\n"  \
-                         "int $0x80\n"      \
-                         "mov %%eax, %0\n"  \
-                         : "=r" (ret)       \
-                         : "i" (num)        \
+# define SYSCALL0(num, ret)                     \
+    __asm__ __volatile__("mov %1, %%eax\n"      \
+                         "int $0x80\n"          \
+                         "mov %%eax, %0\n"      \
+                         : "=r" (ret)           \
+                         : "i" (num)            \
                          : "memory");
 
-# define SYSCALL1(num, arg1, ret)           \
-    __asm__ __volatile__("mov %2, %%ebx\n"  \
-                         "mov %1, %%eax\n"  \
-                         "int $0x80\n"      \
-                         "mov %%eax, %0\n"  \
-                         : "=r" (ret)       \
-                         : "i" (num),       \
-                           "g" (arg1)       \
+# define SYSCALL1(num, arg1, ret)               \
+    __asm__ __volatile__("mov %2, %%ebx\n"      \
+                         "mov %1, %%eax\n"      \
+                         "int $0x80\n"          \
+                         "mov %%eax, %0\n"      \
+                         : "=r" (ret)           \
+                         : "i" (num),           \
+                           "g" (arg1)           \
+                         : "memory");
+
+# define SYSCALL3(num, arg1, arg2, arg3, ret)   \
+    __asm__ __volatile__("mov %4, %%edx\n"      \
+                         "mov %3, %%ecx\n"      \
+                         "mov %2, %%ebx\n"      \
+                         "mov %1, %%eax\n"      \
+                         "int $0x80\n"          \
+                         "mov %%eax, %0\n"      \
+                         : "=r" (ret)           \
+                         : "i" (num),           \
+                           "g" (arg1),          \
+                           "g" (arg2),          \
+                           "g" (arg3)           \
                          : "memory");
 
 #endif /* !LIBC_I386_SYSCALL_H */
