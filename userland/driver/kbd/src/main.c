b23fc0a669caaf9702e8b0a728eb85b3e08258a6
Baptiste Covolato <b.covolato@gmail.com>
2014-08-26 18:03:54 +0200

    [I386][USERLAND][DRV][KBD] Now launch interrupt thread

--- userland/driver/kbd/src/main.c
+++ userland/driver/kbd/src/main.c
@@ -1,7 +1,25 @@
 #include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
+#include <thread.h>
+
+#include <zos/print.h>
+
+#include <arch/interrupt.h>
 
 int main(void)
 {
+    int arch_tid;
+
+    arch_tid = thread_create(interrupt_thread, NULL);
+
+    if (arch_tid < 0)
+    {
+        uprint("Cannot create keyboard interrupt thread ...");
+        return 1;
+    }
+
+    while (1)
+        sleep(1);
+
     return 0;
 }
