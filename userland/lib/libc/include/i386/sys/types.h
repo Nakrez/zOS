5622408844a086581ff5c45319331e907e3683cd
Baptiste Covolato <b.covolato@gmail.com>
2014-08-26 17:53:36 +0200

    [I386][USERLAND][LIBC] Add sys/types.h

---
+++ userland/lib/libc/include/i386/sys/types.h
@@ -0,0 +1,15 @@
+#ifndef I386_SYS_TYPES_H
+# define I386_SYS_TYPES_H
+
+typedef signed char __i8;
+typedef unsigned char __u8;
+typedef signed short __i16;
+typedef unsigned short __u16;
+typedef signed int __i32;
+typedef unsigned int __u32;
+typedef signed long long __i64;
+typedef unsigned long long __u64;
+
+typedef __u32 __size;
+
+#endif /* !I386_SYS_TYPES_H */
