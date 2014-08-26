68807a4309610d43ebcbc21a1d0018da3579accc
Baptiste Covolato <b.covolato@gmail.com>
2014-08-26 17:56:43 +0200

    [USERLAND][LIBC] Add all types to stdint.h

--- userland/lib/libc/include/stdint.h
+++ userland/lib/libc/include/stdint.h
@@ -1,8 +1,17 @@
 #ifndef LIBC_STDINT_H
 # define LIBC_STDINT_H
 
-typedef unsigned int uint32_t;
+# include <sys/types.h>
 
-typedef uint32_t size_t;
+typedef __i8 int8_t;
+typedef __u8 uint8_t;
+typedef __i16 int16_t;
+typedef __u16 uint16_t;
+typedef __i32 int32_t;
+typedef __u32 uint32_t;
+typedef __i64 int64_t;
+typedef __u64 uint64_t;
+
+typedef __size size_t;
 
 #endif /* !LIBC_STDINT_H */
