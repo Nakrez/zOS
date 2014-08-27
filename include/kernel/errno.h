5c13a97a78445cfffd40068322fa9959ffd136e0
Baptiste Covolato <b.covolato@gmail.com>
2014-08-27 15:06:45 +0200

    [KERNEL] Add errno.h

---
+++ include/kernel/errno.h
@@ -0,0 +1,8 @@
+#ifndef ERRNO_H
+# define ERRNO_H
+
+# define EPERM 1 /* Operation not permitted */
+# define ENOENT 2 /* No such file or directory */
+# define ENAMETOOLONG 36 /* File name too long */
+
+#endif /* !ERRNO_H */
