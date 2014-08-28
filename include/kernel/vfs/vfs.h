a3d7268a363f6205ec3a444da72971c739a20c62
Baptiste Covolato <b.covolato@gmail.com>
2014-08-28 22:08:25 +0200

    [KERNEL][VFS] Add vfs_device_create()

--- include/kernel/vfs/vfs.h
+++ include/kernel/vfs/vfs.h
@@ -4,5 +4,6 @@
 # define VFS_MAX_FILENAME_SIZE 255
 
 int vfs_initialize(void);
+int vfs_device_create(const char *name, int pid, int uid, int gid, int perm);
 
 #endif /* !VFS_H */
--- kernel/core/vfs/vfs.c
+++ kernel/core/vfs/vfs.c
@@ -1,5 +1,9 @@
+#include <kernel/errno.h>
+
 #include <kernel/vfs/vfs.h>
 #include <kernel/vfs/path_tree.h>
+#include <kernel/vfs/vdevice.h>
+#include <kernel/vfs/vnode.h>
 
 #include <kernel/kmalloc.h>
 
@@ -9,4 +13,41 @@ int vfs_initialize(void)
         return -1;
 
     return 0;
+}
+
+int vfs_device_create(const char *name, int pid, int uid, int gid, int perm)
+{
+    int res;
+    struct vdevice *device = NULL;
+    struct vnode *node = NULL;
+
+    res = device_create(pid, name, &device);
+
+    if (res < 0)
+        return res;
+
+    node = vnode_create(name, uid, gid, perm,
+                        VFS_TYPE_CHARDEV | VFS_TYPE_FILE);
+
+    if (!node)
+    {
+        res = -ENOMEM;
+
+        goto error;
+    }
+
+    node->dev = device->id;
+
+    res = vtree_insert("/dev", node);
+
+    if (res < 0)
+        goto error;
+
+    return device->id;
+
+error:
+    vnode_destroy(node);
+    device_destroy(pid, device->id);
+
+    return res;
 }
