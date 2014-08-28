94226dade6080bd9260bd6b1e24e4f9e1f8f6bec
Baptiste Covolato <b.covolato@gmail.com>
2014-08-28 15:48:19 +0200

    [KERNEL] Add ENODEV and EINVAL to errno.h

--- include/kernel/errno.h
+++ include/kernel/errno.h
@@ -3,6 +3,8 @@
 
 # define EPERM 1 /* Operation not permitted */
 # define ENOENT 2 /* No such file or directory */
+# define ENODEV 19 /* No such device */
+# define EINVAL 22 /* Invalid argument */
 # define ENAMETOOLONG 36 /* File name too long */
 
 #endif /* !ERRNO_H */
