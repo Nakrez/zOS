2b6b80b8bcc2aca40065a0773087a3a8ff9fa216
Baptiste Covolato <b.covolato@gmail.com>
2014-08-29 18:28:22 +0200

    [USERLAND][BIN][INIT] Add some code to test open() of /dev/kbd

--- userland/bin/init/src/main.c
+++ userland/bin/init/src/main.c
@@ -1,10 +1,22 @@
 #include <unistd.h>
+#include <fcntl.h>
 
 #include <zos/print.h>
 
 int main(void)
 {
+    int fd;
+
     uprint("Init is launching");
+
+    usleep(200);
+
+    fd = open("/dev/kbd", 0, 0);
+
+    if (fd < 0)
+        uprint("Open failed");
+
+    uprint("File opened");
 
     while (1)
         sleep(1);
