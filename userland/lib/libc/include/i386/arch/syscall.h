139ef6592a36896b3f515888b37d367d3083adc1
Baptiste Covolato <b.covolato@gmail.com>
2014-08-29 11:56:11 +0200

    [I386][USERLAND][LIBC] Add SYSCALL5 macro

--- userland/lib/libc/include/i386/arch/syscall.h
+++ userland/lib/libc/include/i386/arch/syscall.h
@@ -14,52 +14,69 @@
 # define SYS_INTERRUPT_UNREGISTER 11
 # define SYS_DEVICE_CREATE 12
 
-# define SYSCALL0(num, ret)                         \
-    __asm__ __volatile__("mov %1, %%eax\n"          \
-                         "int $0x80\n"              \
-                         "mov %%eax, %0\n"          \
-                         : "=r" (ret)               \
-                         : "i" (num)                \
+# define SYSCALL0(num, ret)                                 \
+    __asm__ __volatile__("mov %1, %%eax\n"                  \
+                         "int $0x80\n"                      \
+                         "mov %%eax, %0\n"                  \
+                         : "=r" (ret)                       \
+                         : "i" (num)                        \
                          : "memory");
 
-# define SYSCALL1(num, arg1, ret)                   \
-    __asm__ __volatile__("mov %2, %%ebx\n"          \
-                         "mov %1, %%eax\n"          \
-                         "int $0x80\n"              \
-                         "mov %%eax, %0\n"          \
-                         : "=r" (ret)               \
-                         : "i" (num),               \
-                           "g" (arg1)               \
+# define SYSCALL1(num, arg1, ret)                           \
+    __asm__ __volatile__("mov %2, %%ebx\n"                  \
+                         "mov %1, %%eax\n"                  \
+                         "int $0x80\n"                      \
+                         "mov %%eax, %0\n"                  \
+                         : "=r" (ret)                       \
+                         : "i" (num),                       \
+                           "g" (arg1)                       \
                          : "memory");
 
-# define SYSCALL3(num, arg1, arg2, arg3, ret)       \
-    __asm__ __volatile__("mov %4, %%edx\n"          \
-                         "mov %3, %%ecx\n"          \
-                         "mov %2, %%ebx\n"          \
-                         "mov %1, %%eax\n"          \
-                         "int $0x80\n"              \
-                         "mov %%eax, %0\n"          \
-                         : "=r" (ret)               \
-                         : "i" (num),               \
-                           "g" (arg1),              \
-                           "g" (arg2),              \
-                           "g" (arg3)               \
+# define SYSCALL3(num, arg1, arg2, arg3, ret)               \
+    __asm__ __volatile__("mov %4, %%edx\n"                  \
+                         "mov %3, %%ecx\n"                  \
+                         "mov %2, %%ebx\n"                  \
+                         "mov %1, %%eax\n"                  \
+                         "int $0x80\n"                      \
+                         "mov %%eax, %0\n"                  \
+                         : "=r" (ret)                       \
+                         : "i" (num),                       \
+                           "g" (arg1),                      \
+                           "g" (arg2),                      \
+                           "g" (arg3)                       \
                          : "memory");
 
-# define SYSCALL4(num, arg1, arg2, arg3, arg4, ret) \
-    __asm__ __volatile__("mov %5, %%esi\n"          \
-                         "mov %4, %%edx\n"          \
-                         "mov %3, %%ecx\n"          \
-                         "mov %2, %%ebx\n"          \
-                         "mov %1, %%eax\n"          \
-                         "int $0x80\n"              \
-                         "mov %%eax, %0\n"          \
-                         : "=r" (ret)               \
-                         : "i" (num),               \
-                           "g" (arg1),              \
-                           "g" (arg2),              \
-                           "g" (arg3),              \
-                           "g" (arg4)               \
+# define SYSCALL4(num, arg1, arg2, arg3, arg4, ret)         \
+    __asm__ __volatile__("mov %5, %%esi\n"                  \
+                         "mov %4, %%edx\n"                  \
+                         "mov %3, %%ecx\n"                  \
+                         "mov %2, %%ebx\n"                  \
+                         "mov %1, %%eax\n"                  \
+                         "int $0x80\n"                      \
+                         "mov %%eax, %0\n"                  \
+                         : "=r" (ret)                       \
+                         : "i" (num),                       \
+                           "g" (arg1),                      \
+                           "g" (arg2),                      \
+                           "g" (arg3),                      \
+                           "g" (arg4)                       \
                          : "memory");
 
+# define SYSCALL5(num, arg1, arg2, arg3, arg4, arg5, ret)   \
+    __asm__ __volatile__("mov %6, %%edi\n"                  \
+                         "mov %5, %%esi\n"                  \
+                         "mov %4, %%edx\n"                  \
+                         "mov %3, %%ecx\n"                  \
+                         "mov %2, %%ebx\n"                  \
+                         "mov %1, %%eax\n"                  \
+                         "int $0x80\n"                      \
+                         "mov %%eax, %0\n"                  \
+                         : "=r" (ret)                       \
+                         : "i" (num),                       \
+                           "g" (arg1),                      \
+                           "g" (arg2),                      \
+                           "g" (arg3),                      \
+                           "g" (arg4),                      \
+                           "g" (arg5)                       \
+                         : "memory");
 #endif /* !LIBC_I386_SYSCALL_H */
