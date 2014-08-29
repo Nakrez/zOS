d340982ff6928071bef7b6d4eb5a320bee4c4e08
Baptiste Covolato <b.covolato@gmail.com>
2014-08-29 14:51:13 +0200

    [KERNEL][SCHED] Add SCHED_EV_REQ to event

--- include/kernel/scheduler/event.h
+++ include/kernel/scheduler/event.h
@@ -5,8 +5,15 @@
 # include <kernel/klist.h>
 
 # define SCHED_EV_NONE 0
+
+/* Timer event */
 # define SCHED_EV_TIMER 1
+
+/* Interrupt event */
 # define SCHED_EV_INTERRUPT 2
+
+/* A new request has arrived on a channel */
+# define SCHED_EV_REQ 3
 
 struct scheduler_event {
     int event;
