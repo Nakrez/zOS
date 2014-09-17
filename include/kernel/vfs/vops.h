d96d1d34fbb18aebcfff4d2ee289163aa4a27f63
Baptiste Covolato <b.covolato@gmail.com>
2014-09-17 13:26:24 +0200

    [KERNEL][VFS] Add vfs_lseek()

--- include/kernel/vfs/vops.h
+++ include/kernel/vfs/vops.h
@@ -1,9 +1,14 @@
 #ifndef VFS_VOPS_H
 # define VFS_VOPS_H
 
+# define VFS_SEEK_SET (1 << 0)
+# define VFS_SEEK_CUR (1 << 1)
+# define VFS_SEEK_END (1 << 2)
+
 int vfs_open(const char *pathname, int flags, int mode);
 int vfs_read(int fd, void *buf, size_t count);
 int vfs_write(int fd, const void *buf, size_t count);
 int vfs_close(int fd);
+int vfs_lseek(int fd, int offset, int whence);
 
 #endif /* !VFS_VOPS_H */
--- kernel/core/vfs/vops.c
+++ kernel/core/vfs/vops.c
@@ -333,3 +333,50 @@ error:
 
     return res;
 }
+
+int vfs_lseek(int fd, int offset, int whence)
+{
+    struct process *process = thread_current()->parent;
+
+    if (!(whence & VFS_SEEK_SET) && !(whence && VFS_SEEK_CUR) &&
+        !(whence && VFS_SEEK_END))
+        return -EINVAL;
+
+    if (fd < 0 || fd > PROCESS_MAX_OPEN_FD)
+        return -EINVAL;
+
+    if (!process->files[fd].used)
+        return -EINVAL;
+
+    if (whence & VFS_SEEK_SET)
+    {
+        if (offset < 0)
+            return -EINVAL;
+
+        process->files[fd].offset = offset;
+    }
+    else if (whence & VFS_SEEK_CUR)
+    {
+        size_t old_off = process->files[fd].offset;
+
+        process->files[fd].offset += offset;
+
+        if (offset < 0 && process->files[fd].offset > old_off)
+        {
+            process->files[fd].offset = old_off;
+
+            return -EINVAL;
+        }
+
+        if (offset > 0 && process->files[fd].offset < old_off)
+        {
+            process->files[fd].offset = old_off;
+
+            return -EINVAL;
+        }
+    }
+    else
+        kernel_panic("VFS_SEEK_END not implemented yet");
+
+    return 0;
+}
