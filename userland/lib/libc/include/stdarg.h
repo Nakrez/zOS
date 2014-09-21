65732e54a7fd581cb8624d14add725a6e6245beb
Baptiste Covolato <b.covolato@gmail.com>
2014-09-21 17:42:35 +0200

    [USERLAND][LIBC] Add stdarg.h

---
+++ userland/lib/libc/include/stdarg.h
@@ -0,0 +1,10 @@
+#ifndef LIBC_STDARG_H
+# define LIBC_STDARG_H
+
+typedef __builtin_va_list va_list;
+
+# define va_start(v,l) __builtin_va_start(v,l)
+# define va_end(v) __builtin_va_end(v)
+# define va_arg(v,l) __builtin_va_arg(v,l)
+
+#endif /* !LIBC_STDARG_H */
