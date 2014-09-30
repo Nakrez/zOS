998e179eb55a6967de80251342afdb330c66675c
Baptiste Covolato <b.covolato@gmail.com>
2014-09-30 14:33:25 +0200

    [KERNEL] Add EBUSY and ENOTDIR to errno.h

--- include/kernel/errno.h
+++ include/kernel/errno.h
@@ -6,8 +6,10 @@
 # define EBADF 9 /* Bad file number */
 # define ENOMEM 12 /* Out of memory */
 # define EFAULT 14 /* Bad address */
+# define EBUSY 16 /* Device or resource busy */
 # define EEXIST 17 /* File exists */
 # define ENODEV 19 /* No such device */
+# define ENOTDIR 20 /* Not a directory */
 # define EISDIR 21 /* Is a directory */
 # define EINVAL 22 /* Invalid argument */
 # define EMFILE 24 /* Too many open files */
