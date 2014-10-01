f2179e1b3debb407880002c09b199870085be7cf
Baptiste Covolato <b.covolato@gmail.com>
2014-10-02 01:31:44 +0200

    [KERNEL] Add ENOSYS to errno.h

--- include/kernel/errno.h
+++ include/kernel/errno.h
@@ -14,6 +14,7 @@
 # define EINVAL 22 /* Invalid argument */
 # define EMFILE 24 /* Too many open files */
 # define ENAMETOOLONG 36 /* File name too long */
+# define ENOSYS 38 /* Function not implemented */
 # define ENODATA 61 /* No data available */
 
 #endif /* !ERRNO_H */
