823522caf3c459442e191df4da7a4111fce8ed72
Baptiste Covolato <b.covolato@gmail.com>
2014-10-05 14:59:31 +0200

    [USERLAND] Remove all old vfs related code

--- userland/lib/libc/include/zos/vfs.h
+++ userland/lib/libc/include/zos/vfs.h
@@ -10,30 +10,6 @@
 # define VFS_OPS_CLOSE (1 << 3)
 # define VFS_OPS_UMOUNT (1 << 4)
 
-struct msg_response {
-    uint32_t req_id;
-
-    int ret;
-};
-
-struct open_msg {
-    char *path;
-};
-
-struct rdwr_msg {
-    uint32_t index;
-
-    size_t size;
-
-    size_t off;
-
-    void *data;
-};
-
-struct close_msg {
-    uint32_t index;
-};
-
 /* Lookup request */
 struct req_lookup {
     char *path;
