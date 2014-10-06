a0e347901c67ff3e0d5d1a43f66c804779b2dd6c
Baptiste Covolato <b.covolato@gmail.com>
2014-10-06 14:39:14 +0200

    [USERLAND][BIN] Init now tries to open it configuration file

--- userland/bin/init/src/main.c
+++ userland/bin/init/src/main.c
@@ -5,12 +5,30 @@
 
 int main(void)
 {
+    int timeout = 0;
+    int conf_fd;
+
     uprint("Init is launching");
 
-    usleep(200);
+    while (timeout < 5000)
+    {
+        conf_fd = open("/etc/init_conf", 0, 0);
 
-    while (1)
-        sleep(1);
+        if (conf_fd >= 0)
+            break;
+
+        usleep(50);
+        timeout += 50;
+    }
+
+    if (timeout >= 5000)
+    {
+        uprint("Init failed to open it's configuration file. Aborting...");
+
+        return 1;
+    }
+
+    uprint("Init configuration file open with sucess");
 
     return 0;
 }
