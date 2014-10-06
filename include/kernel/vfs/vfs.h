c716c46a3b770a55e7f112e48fc38495405d6b76
Baptiste Covolato <b.covolato@gmail.com>
2014-10-06 14:13:57 +0200

    [KERNEL][VFS] Add vfs ops definition of MOUNT, LOOKUP, MKDIR and MKNOD

--- include/kernel/vfs/vfs.h
+++ include/kernel/vfs/vfs.h
@@ -8,6 +8,10 @@
 # define VFS_OPS_WRITE (1 << 2)
 # define VFS_OPS_CLOSE (1 << 3)
 # define VFS_OPS_UMOUNT (1 << 4)
+# define VFS_OPS_MOUNT (1 << 5)
+# define VFS_OPS_LOOKUP (1 << 6)
+# define VFS_OPS_MKDIR (1 << 7)
+# define VFS_OPS_MKNOD (1 << 8)
 
 int vfs_initialize(void);
 int vfs_device_create(const char *name, int pid, int uid, int gid, int perm,
