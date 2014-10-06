819cb03c0f555063c41e86e72c9122731d6343b4
Baptiste Covolato <b.covolato@gmail.com>
2014-10-06 14:38:47 +0200

    [MK] Add possibility to add extra files to the rootfs

--- mk/rootfsdirs.mk
+++ mk/rootfsdirs.mk
@@ -1,5 +1,5 @@
 ROOTFSDIRS := rootfs
 ROOTFSDIRS += rootfs/boot rootfs/boot/grub rootfs/boot/grub/i386-pc
-ROOTFSDIRS += rootfs/bin
+ROOTFSDIRS += rootfs/bin rootfs/etc
 
 $(foreach RD, $(ROOTFSDIRS), $(shell mkdir -p $(RD)))
