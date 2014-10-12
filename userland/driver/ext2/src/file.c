78cbaeb9f17919bc38794c91c5b143674de564e7
Baptiste Covolato <b.covolato@gmail.com>
2014-10-12 14:51:55 +0200

    [USERLAND][DRV] Add Ext2 open implementation

--- userland/driver/ext2/src/file.c
+++ userland/driver/ext2/src/file.c
@@ -1,13 +1,23 @@
 #include "file.h"
+#include "inode_cache.h"
 
 int ext2fs_open(struct fiu_internal *fiu, struct req_open *req,
                 struct resp_open *response)
 {
-    (void) fiu;
-    (void) req;
-    (void) response;
+    struct ext2fs *ext2;
+    struct ext2_inode *inode;
 
-    return -1;
+    ext2 = fiu->private;
+
+    if (!(inode = ext2_icache_request(ext2, req->inode)))
+        return -1;
+
+    if (inode->type_perm & EXT2_TYPE_DIRECTORY)
+        return -1;
+
+    response->inode = req->inode;
+
+    return 0;
 }
 
 int ext2fs_close(struct fiu_internal *fiu, struct req_close *req)
