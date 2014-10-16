b52d54c8fd36ce735770574d683c06436facfc0b
Baptiste Covolato <b.covolato@gmail.com>
2014-10-16 22:43:31 +0200

    [KERNEL][I386] Correct bug in arch_back_trace()

--- kernel/arch/i386/back_trace.c
+++ kernel/arch/i386/back_trace.c
@@ -12,6 +12,10 @@ void arch_back_trace(void)
     while (ebp)
     {
         uint32_t eip_stack = ebp + 4;
+
+        if (eip_stack < 0xC0000000)
+            break;
+
         uint32_t eip = *(uint32_t *)eip_stack;
 
         if (eip < 0xC0000000)
