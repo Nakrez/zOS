aee59da43b74b904b5691bc66878521d66fa7f1e
Baptiste Covolato <b.covolato@gmail.com>
2014-10-18 15:57:54 +0200

    [I386][KERNEL] spinlock now disable interrupts and restore eflags

--- include/kernel/arch/i386/arch/spinlock.h
+++ include/kernel/arch/i386/arch/spinlock.h
@@ -1,24 +1,37 @@
 #ifndef ARCH_I386_SPINLOCK_H
 # define ARCH_I386_SPINLOCK_H
 
-# define SPINLOCK_INIT 0
+# include <arch/cpu.h>
 
-typedef int spinlock_t;
+# define SPINLOCK_INIT { 0, 0 }
+
+typedef struct
+{
+    int lock;
+    uint32_t eflags;
+} spinlock_t;
 
 static inline void spinlock_init(spinlock_t *spin)
 {
-    *spin = SPINLOCK_INIT;
+    spin->lock = 0;
+    spin->eflags = 0;
 }
 
 static inline void spinlock_lock(spinlock_t *spin)
 {
-    __asm__ __volatile__ ("1:\n"
+    uint32_t eflags = eflags_get();
+
+    __asm__ __volatile__ ("cli\n"
+                          "1:\n"
                           "mov $0x1, %%eax\n"
                           "lock xchg (%0), %%eax\n"
                           "test %%eax, %%eax\n"
                           "jnz 1b\n"
                           :
-                          : "b" (spin));
+                          : "b" (&spin->lock)
+                          : "memory");
+
+    spin->eflags = eflags;
 }
 
 static inline void spinlock_unlock(spinlock_t *spin)
@@ -26,7 +39,10 @@ static inline void spinlock_unlock(spinlock_t *spin)
     __asm__ __volatile__ ("mov $0x0, %%eax\n"
                           "lock xchg (%0), %%eax\n"
                           :
-                          : "b" (spin));
+                          : "b" (&spin->lock)
+                          : "memory");
+
+    eflags_set(spin->eflags);
 }
 
 #endif /* !ARCH_I386_SPINLOCK_H */
