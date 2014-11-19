a18fc64c173b215319fb0c1fde7077c7ff12245f
Baptiste Covolato <b.covolato@gmail.com>
2014-11-19 13:13:39 +0100

    [I386] Add parsing of MP Configuration Table

--- include/kernel/arch/i386/arch/mp.h
+++ include/kernel/arch/i386/arch/mp.h
@@ -12,6 +12,120 @@
 # define BIOS_ROM_START 0xC00F0000
 # define BIOS_ROM_LEN (0xFFFFF - 0xF0000)
 
+# define MP_PROCESSOR_ENTRY 0
+# define MP_BUS_ENTRY 1
+# define MP_IOAPIC_ENTRY 2
+# define MP_IOINT_ENTRY 3
+# define MP_LOCINT_ENTRY 4
+
+struct mp_locint {
+    uint8_t type;
+
+    uint8_t int_type;
+
+    uint16_t flags;
+
+    uint8_t bus_id;
+
+    uint8_t bus_irq;
+
+    uint8_t lapic_id;
+
+    uint8_t lapic_pin;
+} __attribute__ ((packed));
+
+struct mp_ioint {
+    uint8_t type;
+
+    uint8_t int_type;
+
+    uint16_t flags;
+
+    uint8_t bus_id;
+
+    uint8_t bus_irq;
+
+    uint8_t ioapic_id;
+
+    uint8_t ioapic_pin;
+} __attribute__ ((packed));
+
+struct mp_ioapic {
+    uint8_t type;
+
+    uint8_t id;
+
+    uint8_t version;
+
+    uint8_t flags;
+
+    uint32_t addr;
+} __attribute__ ((packed));
+
+struct mp_bus {
+    uint8_t type;
+
+    uint8_t id;
+
+    char bus_type[6];
+} __attribute__ ((packed));
+
+struct mp_proc {
+    uint8_t type;
+
+    uint8_t lapic_id;
+
+    uint8_t lapic_version;
+
+    uint8_t flags;
+
+    uint32_t signature;
+
+    uint32_t feature;
+
+    uint64_t reserved;
+} __attribute__ ((packed));
+
+struct mp_config {
+    char signature[4];
+
+    uint16_t length;
+
+    uint8_t spec_rec;
+
+    uint8_t checksum;
+
+    char oem_id[8];
+
+    char product_id[12];
+
+    uint32_t oem_table_ptr;
+
+    uint16_t oem_table_size;
+
+    uint16_t entry_count;
+
+    uint32_t local_apic_addr;
+
+    uint16_t ext_table_length;
+
+    uint16_t ext_table_checksum;
+} __attribute__ ((packed));
+
+struct mp_table {
+    char signature[4];
+
+    uint32_t conf_table_ptr;
+
+    uint8_t length;
+
+    uint8_t spec_rev;
+
+    uint8_t checksum;
+
+    uint8_t features[5];
+} __attribute__ ((packed));
+
 void mp_parse_tables(void);
 
 #endif /* !I386_MP_H */
--- kernel/arch/i386/mp.c
+++ kernel/arch/i386/mp.c
@@ -17,9 +17,53 @@ static void *mp_find_magic(void *start, size_t len)
     return NULL;
 }
 
+static void mp_parse_config(struct mp_table *table)
+{
+    struct mp_config *config = (void *)(table->conf_table_ptr + 0xC0000000);
+    uint8_t *type;
+
+    if (strncmp(config->signature, "PCMP", 4))
+        return;
+
+    if (config->oem_table_ptr)
+        console_message(T_ERR, "MP: OEM Table not supported");
+
+    if (config->ext_table_length)
+        console_message(T_ERR, "MP: Extended MP Table not supported");
+
+    type = (void *)(config + 1);
+
+    for (uint16_t i = 0; i < config->entry_count; ++i)
+    {
+        switch (*type)
+        {
+            case MP_PROCESSOR_ENTRY:
+                type += sizeof (struct mp_proc);
+                break;
+            case MP_BUS_ENTRY:
+                type += sizeof (struct mp_bus);
+                break;
+            case MP_IOAPIC_ENTRY:
+                type += sizeof (struct mp_ioapic);
+                break;
+            case MP_IOINT_ENTRY:
+                type += sizeof (struct mp_ioint);
+                break;
+            case MP_LOCINT_ENTRY:
+                type += sizeof (struct mp_locint);
+                break;
+            default:
+                console_message(T_ERR, "MP: Unknown entry %u", *type);
+                return;
+        }
+    }
+
+    return;
+}
+
 void mp_parse_tables(void)
 {
-    void *table;
+    struct mp_table *table;
 
     if ((table = mp_find_magic((void *)EBDA_ADDR, 1024)))
         goto mp_found;
@@ -34,4 +78,11 @@ void mp_parse_tables(void)
 
 mp_found:
     console_message(T_INF, "MP tables found at 0x%x", table);
+
+    if (table->length != 1 || !table->conf_table_ptr)
+        return;
+
+    /* TODO: Checksum */
+
+    mp_parse_config(table);
 }
