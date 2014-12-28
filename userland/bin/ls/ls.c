ecb143044eacd3d49103519422c83eb47d23cacb
Baptiste Covolato <b.covolato@gmail.com>
2014-12-28 12:11:49 +0100

    [USERLAND][BIN] Add ls

---
+++ userland/bin/ls/ls.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <dirent.h>
+
+void print_dir(const char *dirname)
+{
+    DIR *d;
+    struct dirent *dp;
+
+    if (!(d = opendir(dirname)))
+    {
+        fprintf(stderr, "ls: Cannot open %s\n", dirname);
+
+        return;
+    }
+
+    while ((dp = readdir(d)))
+    {
+        fprintf(stdout, "%s\n", dp->d_name);
+    }
+
+    closedir(d);
+}
+
+void usage(void)
+{
+    fprintf(stderr, "ls [FILES]...\n");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        usage();
+
+        return 1;
+    }
+
+    for (int i = 1; i < argc; ++i)
+    {
+        print_dir(argv[i]);
+    }
+
+    return 0;
+}
