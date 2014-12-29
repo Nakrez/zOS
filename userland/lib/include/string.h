b3fb8ceb4d6049b66c54a65e9031e8f94fbb44f4
Baptiste Covolato <b.covolato@gmail.com>
2014-12-29 16:51:24 +0100

    [USERLAND][LIBC] Add strchr()

--- userland/lib/include/string.h
+++ userland/lib/include/string.h
@@ -13,6 +13,7 @@ char *strcpy(char *dest, const char *src);
 int strcmp(const char *str1, const char *str2);
 int strncmp(const char *s1, const char *s2, size_t n);
 char *strcat(char *dest, const char *src);
+char *strchr(const char *s, int c);
 
 void *memcpy(void *dest, const void *src, size_t size);
 void *memset(void *ptr, int value, size_t num);
