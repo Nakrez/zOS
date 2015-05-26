e7f6d39c4bc172ef0964ca78b8157aa2c7dba282
Baptiste Covolato <b.covolato@gmail.com>
2015-05-25 18:08:33 -0700

    [CORE][SEG] Add copyright header

--- include/kernel/mem/segment.h
+++ include/kernel/mem/segment.h
@@ -1,3 +1,29 @@
+/*
+ * zOS
+ * Copyright (C) 2014 - 2015 Baptiste Covolato
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zOS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    include/kernel/mem/segment.h
+ * \brief   Declaration of functions and structures related to physical memory
+ *          management
+ *
+ * \author  Baptiste Covolato
+ */
+
 #ifndef SEGMENT_H
 # define SEGMENT_H
 
