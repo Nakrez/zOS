2c8cb849998dcb41e133b77d0ea468c881bd34df
Baptiste Covolato <b.covolato@gmail.com>
2015-08-12 06:12:02 +0200

    [CORE][FS] Add structure fiu_fs_private

--- include/kernel/fs/fiu.h
+++ include/kernel/fs/fiu.h
@@ -1,10 +1,26 @@
 #ifndef FS_FIU_H
 # define FS_FIU_H
 
+# include <kernel/types.h>
+
 # include <kernel/fs/vfs.h>
 
 extern struct fs_super_operation fiu_fs_super_ops;
 extern struct fs_operation fiu_fs_ops;
 extern struct file_operation fiu_f_ops;
+
+struct channel;
+
+struct fiu_fs_private {
+    /**
+     *  \brief  The channel used to communicate with fs controller
+     */
+    struct channel *master;
+
+    /**
+     *  \brief  Operation supported by the file system
+     */
+    vop_t ops;
+};
 
 #endif /* !FS_FIU_H */
