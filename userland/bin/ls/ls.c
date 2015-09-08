d8c7af6cb3aca583bac255ab58fa58b03dfa3243
Baptiste Covolato <b.covolato@gmail.com>
2015-09-08 12:40:54 +0200

    [USER][BIN] Reformat ls code

--- userland/bin/ls/ls.c
+++ userland/bin/ls/ls.c
@@ -6,15 +6,17 @@ void print_dir(const char *dirname)
     DIR *d;
     struct dirent *dp;
 
-    if (!(d = opendir(dirname)))
-    {
+    d = opendir(dirname);
+    if (!d) {
         fprintf(stderr, "ls: Cannot open %s\n", dirname);
-
         return;
     }
 
-    while ((dp = readdir(d)))
-    {
+    for (;;) {
+        dp = readdir(d);
+        if (!dp)
+            break;
+
         fprintf(stdout, "%s\n", dp->d_name);
     }
 
@@ -28,17 +30,13 @@ void usage(void)
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
+    if (argc == 1) {
         usage();
-
         return 1;
     }
 
     for (int i = 1; i < argc; ++i)
-    {
         print_dir(argv[i]);
-    }
 
     return 0;
 }
